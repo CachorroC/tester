1b7266ac35e4d4622631fcd55bd2428230996789
CachorroC <juankpato87@gmail.com>
2023-11-10 02:38:26 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    Q Please enter the commit message for your changes. Lines starting
    Q with 'Q' will be ignored, and an empty message aborts the commit.
    Q
    Q On branch main
    Q Your branch is up to date with 'origin/main'.
    Q
    Q Changes to be committed:
    Q	modified:   output.txt
    Q	modified:   src/newTester.ts
    Q

--- src/newTester.ts
+++ src/newTester.ts
@@ -1,19 +1,18 @@
 
 import carpetas from '/home/cachorro_cami/Projects/com/rsasesorjuridico/tester/provisionalCarpetas.json';
-import { Carpeta } from './models/carpetas';
 
 const llaves = carpetas.map(
   (
     carp
   ) => {
     const {
-      numero,
-      llaveProceso,
-      idProcesos,
-      deudor
+      llaveProceso, idProcesos, numero
     } = carp;
 
-    return new Carpeta(carp);
+    if ( !llaveProceso ) {
+
+    }
+
   }
 );
 
