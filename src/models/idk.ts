a17821a4c9d1fdbd70c557a70f79af6b6e0cd1b8
CachorroC <juankpato87@gmail.com>
2023-12-07 09:29:25 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    a Please enter the commit message for your changes. Lines starting
    a with 'a' will be ignored, and an empty message aborts the commit.
    a
    a On branch main
    a Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
    a   (use "git pull" to update your local branch)
    a
    a Changes to be committed:
    a	modified:   src/models/idk.ts
    a

--- src/models/idk.ts
+++ src/models/idk.ts
@@ -1,114 +1,70 @@
 import carpetas from '../data/carpetas';
 
-const carpsMap = carpetas.map(
-  (
-    carpeta
-  ) => {
-    return fechaPresentacionBuilder(
-      carpeta.demanda.fechaPresentacion
-    );
-  }
-);
+const carpsMap = carpetas.map((carpeta) => {
+  return fechaPresentacionBuilder(
+    carpeta.demanda.fechaPresentacion
+  );
+});
 
-console.log(
-  carpsMap
-);
+console.log(carpsMap);
 
-function fechaPresentacionBuilder (
-  rawFechaPresentacion? : string | number
+function fechaPresentacionBuilder(
+  rawFechaPresentacion?: string | number
 ) {
-  if ( !rawFechaPresentacion ) {
+  if (!rawFechaPresentacion) {
     return [];
   }
 
   const isNumber = typeof rawFechaPresentacion === 'number';
 
-  if ( isNumber ) {
-    return [
-      new Date(
-        rawFechaPresentacion
-      )
-    ];
+  if (isNumber) {
+    return [new Date(rawFechaPresentacion)];
   }
 
-  const rawFechaPresentacionLength = rawFechaPresentacion.length;
+  const rawFechaPresentacionLength =
+    rawFechaPresentacion.length;
 
-  if ( rawFechaPresentacionLength <= 12 ) {
+  if (rawFechaPresentacionLength <= 12) {
     //* Hay solamente una fecha
-    const fechaFixed = fixSingleFecha(
-      rawFechaPresentacion
-    );
+    const fechaFixed = fixSingleFecha(rawFechaPresentacion);
 
-    if ( !fechaFixed ) {
+    if (!fechaFixed) {
       return [];
     }
 
     return [fechaFixed];
-
   }
 
-  const [
-    firstFecha,
-    secondFecha,
-    ...restFechas
-  ]= rawFechaPresentacion.split(
-    '//'
-  );
+  const [firstFecha, secondFecha, ...restFechas] =
+    rawFechaPresentacion.split('//');
 
-  console.log(
-    firstFecha.length
-  );
+  console.log(firstFecha.length);
 
-  if ( firstFecha.length <= 12 ) {
+  if (firstFecha.length <= 12) {
     //* Es una la primer fecha de presentacion
-    const fechaFixed = fixSingleFecha(
-      firstFecha
-    );
-
+    const fechaFixed = fixSingleFecha(firstFecha);
   }
-
-
-
 }
 
-function fixSingleFecha (
-  rawFecha: string
-) {
-
-  const [
-    rawDay,
-    rawMonth,
-    rawYear
-  ] = rawFecha.trim()
-    .split(
-      '/'
-    );
-  console.log(
-    rawDay
-  );
-  console.log(
-    rawMonth
-  );
-  console.log(
-    rawYear
-  );
+function fixSingleFecha(rawFecha: string) {
+  const [rawDay, rawMonth, rawYear] = rawFecha
+    .trim()
+    .split('/');
+  console.log(rawDay);
+  console.log(rawMonth);
+  console.log(rawYear);
 
   const stringDate = new Date(
-    Number(
-      rawYear
-    ),
-    Number(
-      rawMonth
-    ) - 1, Number(
-      rawDay
-    )
+    Number(rawYear),
+    Number(rawMonth) - 1,
+    Number(rawDay)
   );
 
   console.log(
-    `la nueva fecha del pagaré arrojó: ${ stringDate.toDateString() }`
+    `la nueva fecha del pagaré arrojó: ${stringDate.toDateString()}`
   );
 
-  if ( stringDate.toString() === 'Invalid Date' ) {
+  if (stringDate.toString() === 'Invalid Date') {
     return null;
   }
 
